39c099073d1acd2c753a3a75fdd3d588651bb82c
Ameya Deshmukh <f20200139@pilani.bits-pilani.ac.in>
2022-01-07 11:58:26 +0000

    hardhat initiated

--- components/footer.js
+++ components/footer.js
@@ -11,5 +11,6 @@ export default function Footer() {
       </a>
       <p>Art from Joanne Li @joanne on Figma <a href='https://keeybs.com' className='underline'>keeybs.com</a> <a href='https://creativecommons.org/licenses/by/4.0/' className="underline">CC 4.0</a></p>
     </footer>
+    
   )
 }
