cec3c2f2647eb27fa6d190d5a916ffd8c8bfdc88
Ameya Deshmukh <f20200139@pilani.bits-pilani.ac.in>
2022-01-07 12:32:16 +0000

    script to start the basic contract

---
+++ scripts/start.js
@@ -0,0 +1,16 @@
+async function main(){
+const keyboardsContractFactory = await hre.ethers.getContractFactory("Keyboards");
+const keyboardsContract = await keyboardsContractFactory.deploy();
+await keyboardsContract.deployed();
+console.log("Contract deployed to", keyboardsContract.address);
+const keyboards = await keyboardsContract.getKeyboards();
+console.log("We have the keyboards!", keyboards);
+
+}
+main()
+  .then(()=>process.exit(0))
+  .catch((error)=>{
+    console.error(error);
+    process.exit(1);
+  });
+
